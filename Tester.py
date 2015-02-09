15ff73a5554ecf34ca1f9c1d77a217309cc9214e
pr1438 <philip.rodrigues@solnet.co.nz>
2015-02-09 15:35:29 +1300

    Test for drone - stage 2

--- Tester.py
+++ Tester.py
@@ -5,3 +5,6 @@ import unittest
 class Tester (unittest.TestCase):
     def testAdd(self):
         self.assertEqual(7,7,"Not equal")
+
+    def testAdd2(self):
+        self.assertEqual(9,9,"Not equal")
